9998a0909728aa56d3793daf81a9f4165e7e3ef1
frangusdj <frangusdj@hotmail.com>
2020-05-31 16:06:15 -0500

    Inicio del video

--- src/Lista/MainLista.java
+++ src/Lista/MainLista.java
@@ -5,17 +5,18 @@ public class MainLista {
 	public static void main(String[] args) {
 		ListaEnlazada lista = new ListaEnlazada();//creamos una nueva lista enlazada
 		System.out.println("Esta vacia: "+lista.EstaVacia());//imprimimos haber si la lista esta vacia o no
-		lista.AddPrimero("Franklin");//Añadimos el primer elemento al principio de la lista
-		lista.AddPrimero(20);
-		lista.AddPrimero("Hola que tal");
+		
+		lista.AddPrimero(6);
+		lista.AddPrimero(5);
+		lista.AddPrimero(4);
 		lista.AddPrimero(3);
+		lista.AddPrimero(2);
+		lista.AddPrimero(1);
 		
-		System.out.println("Primer elemento: "+lista.Obtener(0));
+		System.out.println("Primero: "+lista.Obtener(0));
 		System.out.println("Último: "+lista.Obtener(lista.size()-1));
-		System.out.println("Index 2: "+lista.Obtener(2));
-		
-		System.out.println("Esta vacia: "+lista.EstaVacia());
 		System.out.println("Tamaño: "+lista.size());
+		System.out.println("Esta vacia: "+lista.EstaVacia());
 	}
 
 }
